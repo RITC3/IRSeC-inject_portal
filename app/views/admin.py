e5aff78ba00e13690707c7c179be19676d1d2dd5
jgeigerm <jmg2967@rit.edu>
2016-03-28 12:59:15 -0400

    fixed permissions bug

--- app/views/admin.py
+++ app/views/admin.py
@@ -1,8 +1,9 @@
 from flask.ext.admin import AdminIndexView, BaseView
 from flask.ext.admin.contrib.sqla.view import ModelView
-from flask.ext.security import current_user, utils
+from flask_admin.contrib.fileadmin import FileAdmin
+from flask.ext.security import utils
 from wtforms import PasswordField, validators
-from flask import redirect, url_for, flash
+from flask import redirect, url_for, flash, g
 
 """
 AdminBaseView: Access control for the admin panel, without this there is none!
@@ -11,7 +12,7 @@ Parent: flask.ext.admin.BaseView
 class AdminBaseView(BaseView):
     """Who can see the admin page?"""
     def is_accessible(self):
-        if current_user.is_authenticated and current_user.is_admin:
+        if g.user.is_authenticated and g.user.is_admin:
             return True
         return False
 
@@ -25,12 +26,30 @@ class AdminBaseView(BaseView):
 
 
 """
-AdminIndexView: Just overwrite AdminIndexView from flask.ext.admin so that
-                it requires RBAC...
-Parents: flask.ext.admin.AdminIndexView, .AdminBaseView
-I probably should call it something else to avoid confusion...
+WhiteTeamBaseView: Access control for the admin panel, without this there is none!
+Parent: flask.ext.admin.BaseView
 """
-class AdminIndexView(AdminIndexView, AdminBaseView):
+class WhiteTeamBaseView(BaseView):
+    """Who can see the admin page?"""
+    def is_accessible(self):
+        if g.user.is_authenticated and g.user.is_whiteteam:
+            return True
+        return False
+
+    """This is run when a user tries to visit an admin page"""
+    def _handle_view(self, name, **kwargs):
+            # add a check to make sure that the user has permission
+            if not self.is_accessible():
+                # if not then print an error and redirect
+                flash("You don't have permission to go there", category="warning")
+                return redirect(url_for('main.index'))
+
+
+"""
+WhiteTeamIndexView: Make AdminIndexView from flask.ext.admin require RBAC
+Parents: flask.ext.admin.AdminIndexView, .WhiteTeamBaseView
+"""
+class ProtectedIndexView(AdminIndexView, WhiteTeamBaseView):
     pass
 
 
@@ -40,6 +59,19 @@ Parents: flask.ext.admin.contrib.sqla.view.ModelView, .AdminBaseView
 """
 class AdminModelView(ModelView, AdminBaseView):
     pass
+
+
+"""
+WhiteTeamModelView: Add RBAC to flask-admin's model view
+Parents: flask.ext.admin.contrib.sqla.view.ModelView, .AdminBaseView
+"""
+class WhiteTeamModelView(ModelView, WhiteTeamBaseView):
+
+    def is_accessible(self):
+        self.can_create = g.user.is_admin
+        self.can_edit = g.user.is_admin
+        self.can_delete = g.user.is_admin
+        return super(WhiteTeamModelView, self).is_accessible()
 
 
 """
@@ -53,7 +85,7 @@ class UserModelView(AdminModelView):
     # this information should not be changed, so don't make it editable
     form_excluded_columns = ['last_login_at', 'current_login_at',
                              'last_login_ip', 'current_login_ip',
-                             'login_count']
+                             'login_count', 'submissions']
 
     # make sure the password can't be seen when typing it
     form_overrides = dict(password=PasswordField)
@@ -77,3 +109,23 @@ Parent: .AdminModelView
 """
 class RoleModelView(AdminModelView):
     pass
+
+
+"""
+InjectModelView: The model view for Injects
+Parent: .AdminModelView
+"""
+class InjectModelView(WhiteTeamModelView):
+    pass
+
+
+"""
+WhiteTeamFileAdmin: The file admin view for shared files
+Parents: FileAdmin, WhiteTeamBaseView
+"""
+class WhiteTeamFileAdmin(FileAdmin, WhiteTeamBaseView):
+
+    def is_accessible(self):
+        self.can_delete = g.user.is_admin
+        self.can_delete_dirs = g.user.is_admin
+        return super(WhiteTeamFileAdmin, self).is_accessible()
